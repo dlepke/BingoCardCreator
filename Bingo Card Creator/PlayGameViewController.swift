5aceebf2f88d945c0ebe25f470f928bf9648fdf5
dlepke <deannalepke@gmail.com>
2019-01-14 19:50:42 -0800

    got bingo game page started, laid out buttons and formatted - not functional yet

--- Bingo Card Creator/PlayGameViewController.swift
+++ Bingo Card Creator/PlayGameViewController.swift
@@ -11,9 +11,21 @@ import UIKit
 
 class PlayGameViewController: UIViewController {
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor(patternImage: backgroundGradientImage(bounds: view.bounds))
+        
+        for button in BingoCardButtons {
+            button.contentEdgeInsets = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+            button.titleLabel?.textAlignment = .center
+            button.layer.borderColor = UIColor.white.cgColor
+            button.layer.borderWidth = 1
+            
+        }
+        
     }
+    @IBOutlet var BingoCardButtons: [UIButton]!
 }
